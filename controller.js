9998c6f5292c02132567525cbb961936f3aee78e
Pallav Jain <pallavjain409@gmail.com>
2019-05-22 10:54:23 +0530

    s

--- controller.js
+++ controller.js
@@ -1,25 +1,23 @@
-
-
-player-1 is X and player-2 is 0;
+player - 1 is X and player - 2 is 0;
 var totalMovesPlayed = 0;
 var playerWins = false;
 var turn = true;
 do {
-player = getting player whose turn is from whoseTurn(turn);
-turn = !turn;
-getting move played by player1 or player2  from getMove(player);
-move = getMove(player);
-applying move played by player in inner html by addPlayersMoveInHtml(move);
-totalMovesPlayed ++;
-if (totalMovesPlayed <= 5){
-  if checkifThereIsWinner(player) is true{
-    displayResult(player,wins)
-    playerWins = true;
-    exit the loop
+  player = getting player whose turn is from whoseTurn(turn);
+  turn = !turn;
+  getting move played by player1 or player2 from getMove(player);
+  move = getMove(player);
+  applying move played by player in inner html by addPlayersMoveInHtml(move);
+  totalMovesPlayed++;
+  if (totalMovesPlayed <= 5) {
+    if checkifThereIsWinner(player) is true {
+      displayResult(player, wins)
+      playerWins = true;
+      exit the loop
+    }
   }
 }
-}
-while(totalMovesPlayed is not equal to nine and playerwin is false)
-if (playerWins == false){
-  displayResult(player,draw);
+while (totalMovesPlayed is not equal to nine and playerwin is false)
+if (playerWins == false) {
+  displayResult(player, draw);
 }
--- model.js
+++ model.js
@@ -13,63 +13,68 @@ var winningCombinations = [
 var totalMovesPlayed;
 
 function whoseTurn(turn) {
-  if (turn == true){
+  if (turn == true) {
     return player1;
-  }
-  else{
+  } else {
     return player2;
   }
 }
 
 function gettingMove(player) {
-  if (player == player1)
-  {
-    we will listen for click by adding eventlistner
+  if (player == player1) {
+    we will listen
+    for click by adding eventlistner
     after clicking we will get move
     move = e.value;
-    check for move available from isMoveAvailable(move);
-    if isMoveAvailable returns true{
-       we will push move in player1Moves;
-       increment totalMovesPlayed;
-       return move
-     }
-        else {
-          gettingMove(player1);
-        }
-  }
-  else {
+    check
+    for move available from isMoveAvailable(move);
+    if isMoveAvailable returns true {
+      we will push move in player1Moves;
+      increment totalMovesPlayed;
+      return move
+    } else {
+      gettingMove(player1);
+    }
+  } else {
     move = getBestMove();
-    if(isMoveAvailable(move)){
+    if (isMoveAvailable(move)) {
       push move in players2 move
       increment totalMovesPlayed
       return move;
-    }
-    else{
+    } else {
       gettingMove(player2)
     }
   }
-  }
+}
 
 function isMoveAvailable(move) {
-  it check for move available by checking classes of div element and if does not have class played then only it will return true else it will return false
+  it check
+  for move available by checking classes of div element and
+  if does not have class played then only it will
+  return true
+  else it will
+  return false
 }
-function getBestMove(){
+
+function getBestMove() {
 
 }
-function addPlayersMoveInHtml(move){
+
+function addPlayersMoveInHtml(move) {
   change the innerHTML of square corresponding to move;
-add played class to square corresponding to that move;
- }
-function checkifThereIsWinner(player){
+  add played class to square corresponding to that move;
+}
+
+function checkifThereIsWinner(player) {
   compare the array of current player with winningCombinations
-  if (there is winner){
+  if (there is winner) {
     return true;
   }
 }
- function displayResult(player, result){
-   if result is winner{
-     display it in html with player
-   }
-else
-   display result draw in html
- }
+
+function displayResult(player, result) {
+  if result is winner {
+    display it in html with player
+  } else
+    display result draw in html
+}
