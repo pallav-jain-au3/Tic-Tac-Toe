d462c4f3e07aa8c6a0c1ba438f1d3ad03c0027a3
Pallav Jain <pallavjain409@gmail.com>
2019-06-02 08:12:48 +0530

    game flow completed

--- controller.js
+++ controller.js
@@ -1,45 +1,76 @@
-const squares = document.querySelectorAll('.square');
-function initialize(){
-  initializeModel();
-  initializeView();
+function initializeGame() {
+  initializeSeries();
   startGame();
 }
-function initializeModel(){
-  turn = true;
+initializeGame();
+
+function startGame() {
+  addClickListenersToCells();
+  displayScores(getScoreForPlayerP1(), getScoreForPlayerP2());
+}
+
+function cellTapped(event) {
+  const gridNumber = event.target.id;
+  const player = whoseTurnNext();
+  event.target.classList.add(player == players.player1 ? "zero" : "cross");
+  aMoveMade(player, gridNumber);
+  const gameStatus = getGameResultStatus(player);
+  if (gameStatus != GameStatus.notEnded) {
+    endGame(gameStatus);
+  } else {
+    $("#" + gridNumber).off('click', cellTapped);
+  }
+}
+
+function endGame(gameStatus) {
+  displayWinningMoves(getWinningArray());
+  displayGameResult(gameStatus);
+  makeScreenUnTappable();
+  updatePlayerScore(gameStatus);
+  displayScores(getScoreForPlayerP1(), getScoreForPlayerP2());
+  }
+  function displayWinningMoves(winningArray){
+    for(let gridNumber of winningArray){
+      console.log(gridNumber);
+      $('#' + gridNumber).addClass('winning-moves');
+    }
+  }
+
+function makeScreenUnTappable() {
+  $(".square").off('click', cellTapped);
+}
+
+function askUserForNextGame() {
+  if (confirm("Do you want to play 1 more game?")) {
+    resetGame();
+
+  }
+}
+function resetGame(){
+  initializeView();
+  initializeGame();
+}
+function addClickListenersToCells() {
+  $(".square").on('click', cellTapped);
+}
+
+function displayScores(player1Score, player2Score) {
+  $("#player1score").text(player1Score);
+  $("#player2score").text(player2Score);
+}
+
+function displayGameResult(gameResult) {
+  console.log(getGameResultString(gameResult))
+  $('.gameResult h1').text(getGameResultString(gameResult));
+
 }
 function initializeView(){
-  squares.forEach(square => square.classList.add('notClicked'));
-  squares.forEach(square => square.innerHTML = "");
+  clearBoard();
+  resetResults();
 }
-function startGame(){
-  player = whoseTurn();
-  displayTurn(player);
-  let cells = document.querySelectorAll('.notClicked');
-  cells.forEach(cell => cell.addEventListener('click', cellTapped));
+function clearBoard(){
+  $('.square').removeClass('zero cross winning-moves');
 }
-
-
-function cellTapped(e){
-gridNumber = e.target.id;
- console.log(gridNumber);
-displaySymbolInCell(gridNumber);
-removeListener(gridNumber);
-
+function resetResults(){
+  $('.gameResult h1').text("");
 }
-function removeListener(gridNumber){
-  let square = document.getElementById(gridNumber);
-  console.log(square);
-  square.removeEventListener('click', function (){
-    console.log("removed");
-  })
-}
-function displaySymbolInCell(gridNumber){
-  document.getElementById(gridNumber).innerHTML = "x";
-}
-function whoseTurn(){}
-
-function displayTurn(player){
-  document.querySelector('.whose-turn h1').innerHTML = player;
-}
-
-initialize();
--- model.js
+++ model.js
@@ -1,3 +1,5 @@
+let player1Moves;
+let player2Moves;
 const winningCombinations = [
   ["0", "3", "6"],
   ["1", "4", "7"],
@@ -8,20 +10,102 @@ const winningCombinations = [
   ["0", "4", "8"],
   ["6", "4", "2"]
 ];
-var player1Moves = [];
-var player2Moves = [];
+let player1Score = 0;
+let player2Score = 0;
+let moves = [];
 
-var playersMove;
-
-function initialize(){
-  playersMove = [0, 1, 2, 3, 4 ,5, 6, 7, 8];
-  turn = true ;
+let winningArray;
+function initializeGameModel() {
+  moves = [];
+  player1Moves = [];
+  player2Moves = [];
+  winningArray = [];
 }
-function whoseTurn(){
-  return player1;
-  //return numberOfMoves%2 == 0  ? player1: player2;
+function getScoreForPlayerP1(){
+  return player1Score;
+}
+function getScoreForPlayerP2(){
+  return player2Score;
+}
+function initializeSeries() {
+  initializeGameModel()
 }
 
-function aMovesisMade(gridNumber, player){
+function whoseTurnNext() {
+  return moves.length % 2 == 0 ? players.player1 : players.player2;
+}
 
+function aMoveMade(player, gridNumber) {
+  moves.push(gridNumber);
+  if (player == 1) {
+    player1Moves.push(gridNumber);
+
+  } else {
+    player2Moves.push(gridNumber);
   }
+
+}
+
+
+
+const players = {
+  player1: 1,
+  player2: 2
+}
+
+const GameStatus = {
+  draw: 0,
+  player1: 1,
+  player2: 2,
+  notEnded: -1
+};
+
+function getGameResultStatus(player) {
+  if (moves.length > 4) {
+      const hasAnybodyWon = player == players.player1 ? areWinningMoves(player1Moves): areWinningMoves(player2Moves);
+      if(hasAnybodyWon) {
+        return player == players.player1 ?GameStatus.player1 : players.player2;
+      } else if(moves.length == 9) {
+        return GameStatus.draw;
+      }
+  }
+  return GameStatus.notEnded;
+}
+
+// input moves = ["1", "2", "3"]
+// output bool
+function areWinningMoves(moves) {
+    const isWinningMoveInUserMoves = winingMove => moves.includes(winingMove)
+    const isMoveInCurrentWinningCombination = winningCombination => winningCombination.every(isWinningMoveInUserMoves)
+    const winningMoves = winningCombinations.find(isMoveInCurrentWinningCombination);
+
+     if(!!winningMoves){
+       winningArray = winningMoves;
+     }
+     return !!winningMoves;
+}
+
+
+function getGameResultString(gameResult) {
+  let gameResultString;
+  switch(gameResult) {
+    case GameStatus.player1: gameResultString = "Player 1 Won!!!";
+    break;
+    case GameStatus.player2: gameResultString = "Player 2 Won!!!";
+    break;
+    case GameStatus.draw: gameResultString = "Game Draw";
+    break;
+  }
+  return gameResultString;
+}
+function updatePlayerScore(gameStatus){
+  switch (gameStatus){
+    case GameStatus.player1 :player1Score += 1;
+    break;
+    case GameStatus.player2 :player2Score += 1;
+    break;
+    }
+}
+function getWinningArray(){
+  return winningArray;
+}
